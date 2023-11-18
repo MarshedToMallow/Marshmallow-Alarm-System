ebfaafba9ce844f5e2abfdc353689231cfaa4805
johnpmarshall2000 <johnpmarshall2000@gmail.com>
2023-11-18 09:46:51 -0500

    Updated handler

--- alarms.py
+++ alarms.py
@@ -3,8 +3,9 @@ import datetime
 NEVER = datetime.timedelta(weeks = 10 ** 8)
 
 class Alarm:
-    def __init__(self, time, repeat = NEVER, offset = datetime.timedelta()):
+    def __init__(self, time: datetime.datetime, name: str = "Alarm", repeat: datetime.timedelta = NEVER, offset: datetime.timedelta = datetime.timedelta()):
         self.time = time
+        self.name = name
         self.repeat = repeat
         self.offset = offset
     
@@ -13,9 +14,21 @@ class Alarm:
         if trigger and self.repeat != NEVER:self.time += self.repeat
         return trigger
 
+class RelativeAlarm:
+    def __init__(self, parent_offset: datetime.timedelta, offset: datetime.timedelta = datetime.timedelta()):
+        self.parent_offset = parent_offset
+        self.offset = offset
+    
+    def parent_triggered(self, parent_time):
+        self.time = parent_time + self.parent_offset + self.offset
+
 class AlarmHandler:
     def __init__(self):
         self.alarms = []
 
-    def add_alarm(self, time, repeat = datetime.timedelta(365 * 1000), offset = datetime.timedelta()):
-        self.alarms.append(Alarm(time, repeat, offset))
+    def add_alarm(self, time: datetime.date, name: str = "Alarm", repeat = datetime.timedelta(365 * 1000), offset = datetime.timedelta()):
+        self.alarms.append(Alarm(time, name, repeat, offset))
+    
+    def check_alarms(self, early: datetime.datetime, late: datetime.datetime):
+        for alarm in self.alarms:
+            if alarm.check(early, late):print(alarm.name)
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import datetime
-from alarms import Alarm
+from alarms import AlarmHandler
 
 SECOND = datetime.timedelta(seconds = 1)
 
-target = Alarm(datetime.datetime.now() + datetime.timedelta(seconds = 5), datetime.timedelta(seconds = 10))
-early = target.time
+handler = AlarmHandler()
+handler.add_alarm(time = datetime.datetime.now() + datetime.timedelta(seconds = 5), name = "BEEP BEEP BEEP", repeat = datetime.timedelta(seconds = 10))
+early = handler.alarms[0].time
 
 while True:
     late = datetime.datetime.now()
-    if target.check(early, late):print("BEEP BEEP BEEP")
-    if early.microsecond > late.microsecond:print("A second has passed")
+    handler.check_alarms(early, late)
+    if early.microsecond > late.microsecond:print(".")
     early = late
